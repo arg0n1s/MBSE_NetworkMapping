c424d8d8f94cbdfbc8447a8e6260402fa1736c64
Nsteel <nicoacero94@gmail.com>
2017-09-06 11:59:05 +0200

    yey!!

--- src/TGG/ReqToImpTransformatorCorr.java
+++ src/TGG/ReqToImpTransformatorCorr.java
@@ -7,7 +7,6 @@ import org.moflon.tgg.algorithm.synchronization.SynchronizationHelper;
 
 import ReqToImpTransformator.ReqToImpTransformatorPackage;
 import ReqToImpTransformator.org.moflon.tie.CustomILPObjectiveProvider;
-import ReqToImpTransformator.org.moflon.tie.ReqToImpTransformatorConsistencyCheck;
 
 public class ReqToImpTransformatorCorr extends SynchronizationHelper {
 
@@ -18,24 +17,23 @@ public class ReqToImpTransformatorCorr extends SynchronizationHelper {
 	public void runCorrelation(File srcPath, File trgPath) throws Exception{
 		// Set up logging
         BasicConfigurator.configure();
-        
-        ReqToImpTransformatorConsistencyCheck helper = new ReqToImpTransformatorConsistencyCheck();
-        helper.loadSrc("Models/"+srcPath.getName());
-		helper.loadTrg("Models/"+trgPath.getName());
 
-		helper.setUserDefiendILPConstraintProvider(new CorrILPConstraintProvider());
-		helper.setUserDefiendILPObjectiveProvider(new CustomILPObjectiveProvider());
-		helper.createCorrespondences(true);
+        this.loadSrc("Models/"+srcPath.getName());
+		this.loadTrg("Models/"+trgPath.getName());
+
+		this.setUserDefiendILPConstraintProvider(new CorrILPConstraintProvider());
+		this.setUserDefiendILPObjectiveProvider(new CustomILPObjectiveProvider());
+		this.createCorrespondences(true);
 		
 		
 		//src and trg models are modified when preparing deltas.
 		//save all files in a separate location
 		String srcName = srcPath.getName().substring(0, srcPath.getName().length()-4);
 		String trgName = trgPath.getName().substring(0, trgPath.getName().length()-4);
-		helper.saveSrc("Correlations/"+srcName+"-"+trgName+"-req.xmi");
-		helper.saveTrg("Correlations/"+srcName+"-"+trgName+"-impl.xmi");
-		helper.saveCorr("Correlations/"+srcName+"-"+trgName+"-corr.xmi");
-		helper.saveConsistencyCheckProtocol("Correlations/"+srcName+"-"+trgName+"-protocol.xmi");
+		this.saveSrc("Correlations/"+srcName+"-"+trgName+"-req.xmi");
+		this.saveTrg("Correlations/"+srcName+"-"+trgName+"-impl.xmi");
+		this.saveCorr("Correlations/"+srcName+"-"+trgName+"-corr.xmi");
+		this.saveConsistencyCheckProtocol("Correlations/"+srcName+"-"+trgName+"-protocol.xmi");
 	}
 
 }
